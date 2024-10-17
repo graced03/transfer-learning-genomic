91b5459923022fcb29bba4a84cf45c109c12c73c
graced03 <dxy_03@outlook.com>
2024-10-17 08:49:10 -0400

    update the script to read in snps selected by clumping

--- R/read_in_clump_snps.R
+++ R/read_in_clump_snps.R
@@ -18,20 +18,66 @@ library(Matrix)
 # 
 # dim(pvar_table) # 13355     6
 
-# clump_snp <- fread("./data/LDL/clump/irish_snp_clump/ldl_irish_chr1_plink_clumped.clumps")
+clump_snp <- fread("./data/LDL/clump/british_snp_clump/ldl_british_chr1_plink_clumped.clumps")
 
-chr_snp_list <- list()
-snp_count <- 0
+########################### Snps selected by gwas on both ######################
+############################## British and Irish ###############################
+
+british_irish_chr_snp_list <- list()
+british_irish_snp_count <- 0
+for (chr in 1:22){
+  clump_snp_table <- fread(paste0("./data/LDL/clump/british_irish_snp_clump/ldl_british_irish_chr",
+                                  as.character(chr),
+                                  "_plink_clumped.clumps"))
+  british_irish_chr_snp_list[[paste0("chr",as.character(chr))]] <- clump_snp_table$ID
+  british_irish_snp_count <- british_irish_snp_count + length(clump_snp_table$ID)
+}
+
+
+################################## British #####################################
+
+british_chr_snp_list <- list()
+british_snp_count <- 0
+for (chr in 1:22){
+  clump_snp_table <- fread(paste0("./data/LDL/clump/british_snp_clump/ldl_british_chr",
+                                  as.character(chr),
+                                  "_plink_clumped.clumps"))
+  british_chr_snp_list[[paste0("chr",as.character(chr))]] <- clump_snp_table$ID
+  british_snp_count <- british_snp_count + length(clump_snp_table$ID)
+}
+
+################################## Irish #######################################
+
+irish_chr_snp_list <- list()
+irish_snp_count <- 0
 for (chr in 1:22){
   clump_snp_table <- fread(paste0("./data/LDL/clump/irish_snp_clump/ldl_irish_chr",
                                   as.character(chr),
                                   "_plink_clumped.clumps"))
-  chr_snp_list[[paste0("chr",as.character(chr))]] <- clump_snp_table$ID
-  snp_count <- snp_count + length(clump_snp_table$ID)
+  irish_chr_snp_list[[paste0("chr",as.character(chr))]] <- clump_snp_table$ID
+  irish_snp_count <- irish_snp_count + length(clump_snp_table$ID)
 }
 
+########################### Look up the intersection############################
+
+
+british_chr_snps <- Reduce(c, british_chr_snp_list)
+irish_chr_snps <- Reduce(c, irish_chr_snp_list)
+british_irish_chr_snps <- Reduce(c, british_irish_chr_snp_list)
+
+length(intersect(british_chr_snps, british_irish_chr_snps)) # 2904
+length(intersect(british_chr_snps, irish_chr_snps)) # 56
+length(intersect(british_irish_chr_snps, irish_chr_snps)) # 58
+
+length(union(british_chr_snps, british_irish_chr_snps)) # 3688
+length(union(british_chr_snps, irish_chr_snps)) # 3508
+length(union(british_irish_chr_snps, irish_chr_snps)) # 3586
+length(union(union(british_irish_chr_snps, irish_chr_snps),british_chr_snps)) # 3937
+
+
+########################### Extract the SNP data ###############################
 # Phenotype file
-phenotype <- readRDS("~/UKBB/data/LDL/dat_LDL_British_Irish.rds")
+phenotype <- readRDS("~/UKBB/data/LDL/phenotype/dat_LDL_British_Irish.rds")
 # Genotype file directory
 geno_path <- "./data/geno/"
 
@@ -48,9 +94,9 @@ for (i in 1:22){
   pvar_table <- fread(pvar_path)
   pvar_table$Index <- 1:nrow(pvar_table)
   
-  snp_selected_chr <- pvar_table %>% filter(ID %in% chr_snp_list[[chr]])
+  snp_selected_chr <- pvar_table %>% filter(ID %in% british_chr_snp_list[[chr]])
   geno_mat <- ReadList(pgen, snp_selected_chr$Index, meanimpute=F)
-  colnames(geno_mat) <- chr_snp_list[[chr]]
+  colnames(geno_mat) <- british_chr_snp_list[[chr]]
   
   merged <- cbind(sample_data[,2], geno_mat)
   phenotype <- merge(phenotype, merged, by = "IID", all.x = TRUE)
@@ -65,27 +111,10 @@ dim(phenotype)[1] * dim(phenotype)[2]
 
 dim(phenotype)
 View(phenotype)
-saveRDS(phenotype, file = "~/UKBB/data/LDL/dat_LDL_British_Irish_pheotype_genotype_irish_gwas.rds")
 
-phenotype <- readRDS("~/UKBB/data/LDL/dat_LDL_British_Irish_pheotype_genotype_irish_gwas.rds")
+# saveRDS(phenotype, file = "~/UKBB/data/LDL/clump_selected_snp_w_phenotype/british_irish_snp_clump/dat_LDL_British_Irish_pheotype_genotype_british_irish_gwas.rds")
+saveRDS(phenotype, file = "/fastscratch/myscratch/xding/UKBB/dat_LDL_British_Irish_pheotype_genotype_british_gwas.rds")
 
-phenotype$sex <- as.numeric(phenotype$sex) - 1
-typeof(phenotype$sex)
-phenotype_british <- phenotype[which(phenotype$ethnicity == "British"),]
-phenotype_irish <- phenotype[which(phenotype$ethnicity == "Irish"),]
-
-X_british <- phenotype_british[,-c("#FID","IID","ethnicity","LDL","used.in.pca.calculation")]
-X_irish <- phenotype_irish[,-c("#FID","IID","ethnicity","LDL","used.in.pca.calculation")]
-
-X_british_matrix <- as.matrix(X_british)
-X_british_matrix_sparse <- Matrix(X_british_matrix, sparse = TRUE)
-
-saveRDS(X_british_matrix_sparse, "~/UKBB/data/LDL/X_british_matrix_sparse_LDL_pheotype_genotype_irish_gwas.rds")
-
-head(X)
-dim(X)
-
-X[,1]
 
 
 
